074cf3f08c4f8133c27552db74dfe4b369278c06
Leonardo Henrique <code.leoh@gmail.com>
2025-01-02 20:29:27 -0300

    Adicionando o nome, descrição, preço e imagem da pizza e construindo o modal

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ const cs = (el) => document.querySelectorAll(el);
 
 pizzaJson.map((item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
-    // Preencher as informações em pizzaItem
+    const pizzaPrice = item.price.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
 
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `${pizzaPrice}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+
+        c('.pizzaWindowArea').style.display = 'flex';
+    });
+    
     c('.pizza-area').append(pizzaItem);
 })
