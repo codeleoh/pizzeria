7161523bef8875a14567fa7e5224b5b621b5503d
Leonardo Henrique <code.leoh@gmail.com>
2025-01-10 15:11:33 -0300

    Botão de tamanho funcionando e botão de adicionar as informações da pizza selecionada no carrinho em construção

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQt = 1; 
+let modalkey = 0;
 
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
@@ -22,12 +24,13 @@ pizzaJson.map((item, index) => {
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
+        modalkey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
-        c('.pizzaInfo--size.selected').classList.remove('selected')
+        c('.pizzaInfo--size.selected').classList.remove('selected');
         cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
             if(sizeIndex == 2) {
                 size.classList.add('selected')
@@ -68,4 +71,21 @@ c('.pizzaInfo--qtmenos').addEventListener('click', () => {
 c('.pizzaInfo--qtmais').addEventListener('click', () => {
     modalQt++;
     c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e) => {
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
+c('.pizzaInfo--addButton').addEventListener('click', () => {
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+
+    cart.push({
+        id: pizzaJson[modalkey].id,
+        size,
+        qt: modalQt
+    });
+
+    closeModal();
 });
